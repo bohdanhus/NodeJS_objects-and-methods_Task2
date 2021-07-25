778e437135873d452197527bd55627db5c21c1d1
bromanovich99 <bromanovich99@gmail.com>
2021-07-25 14:42:46 +0300

    print board'

--- Task.test.js
+++ Task.test.js
@@ -1,1 +1,13 @@
-const task = require('./objectAndMethod')
+const task = require('./objectAndMethod');
+
+
+describe("object", () => {
+    it("toggle test", () => {
+        task.toggle();
+        expect(task.done).toBe(false);
+    });
+    it("isoverdue test"), () => {
+        task.isOverdue();
+        expect(task.dueDate).toBe(true);
+    }
+});
